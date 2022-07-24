5271c787164db587c7ecb2ddbcb284bfd2da4e0e
omaran bazna <omran4d@gmail.com>
2022-07-23 22:59:17 -0400

    write  the code

--- EqualRowandColumnPairs.js
+++ EqualRowandColumnPairs.js
@@ -7,4 +7,25 @@ var equalPairs = function (grid) {
   //for every row loop through the columns
   //compare the row and column and break comparing once finding mismatch
   //add to count if match
+
+  let count = 0;
+  let match = false;
+  for (let row of grid) {
+    for (let col = 0; col < grid.length; col++) {
+      ///start comparing th row and col
+
+      match = true;
+
+      for (let el = 0; el < grid.length; el++) {
+        if (row[el] == grid[el][col]) {
+          match = false;
+          break;
+        }
+      }
+      if (match) {
+        count++;
+      }
+    }
+  }
+  return count;
 };
