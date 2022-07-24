4d7c5fb4e767750a5f1c1e132f21cc656f9bb235
omaran bazna <omran4d@gmail.com>
2022-07-23 22:49:47 -0400

    add pesudocodefor the second problem

--- EqualRowandColumnPairs.js
+++ EqualRowandColumnPairs.js
@@ -2,4 +2,9 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var equalPairs = function (grid) {};
+var equalPairs = function (grid) {
+  // loop through the rows
+  //for every row loop through the columns
+  //compare the row and column and break comparing once finding mismatch
+  //add to count if match
+};
